53e0c19bc549f83aa5217483344e41515a9c8052
Rubi khatri <91728018+rubeyyyy@users.noreply.github.com>
2021-10-02 17:32:00 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// WEATHER APP
+let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+
+/**
+ * Retriving data from the url
+ */
+getWeatherData = (city) => {
+  const URL = "https://api.openweathermap.org/data/2.5/weather";
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+  const weatherPromise  = fetch(FULL_URL);
+  return weatherPromise.then((response) => {
+    return response.json();
+  })
+}
+
+/**
+ * Retrieving city and getting the weather data
+ */
+searchCity = () => {
+  const city = document.getElementById('city-input').value;
+  getWeatherData(city)
+  .then((res)=>{
+    showWeatherData(res);
+  }).catch((error)=>{
+    console.log(error);
+    
+  })
+}
+
+/**
+ * Show the weather in the web page
+ */
+showWeatherData = (weatherData) => {
+  document.getElementById("city-name").innerText = weatherData.name;
+  document.getElementById("weather-type").innerText = weatherData.weather[0].main;
+  document.getElementById("temp").innerText = weatherData.main.temp;
+  document.getElementById("min-temp").innerText = weatherData.main.temp_min;
+  document.getElementById("max-temp").innerText = weatherData.main.temp_max;
+  document.getElementById("weather-output").classList.add('visible');
+}
+
